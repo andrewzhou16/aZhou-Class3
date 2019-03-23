fc76fab57d81ed1b2f5a8ba210ea97ae5e4a41d7
andrewzhou16 <48416885+andrewzhou16@users.noreply.github.com>
2019-03-23 18:53:27 -0400

    Add files via upload

---
+++ Lesson1/hw.java
@@ -0,0 +1,53 @@
+
+public class lesson{
+public static void main(String[] args){
+  int sum = 0;
+  for (int i=0; i<51; i++) {
+    sum = sum + i;
+  }
+  System.out.println(sum)
+}
+public class lesson{
+public static void main(String[] args){
+  for (int x = 1; x <= 100; x++){
+    if (x%5 == 0){
+    System.out.println(x-1);
+    System.out.printlen(x+1);
+    }
+  }
+}
+}
+public class lesson{
+public static void main(String[] args){
+  int y=
+  if (y<=60){
+    System.out,println("F");
+  }
+  if (y>60 && y<=70){
+    System.out.prinln("D");
+  }
+  if (y>70 && y<=80){
+    System.out.println("C");
+  }
+  if (y>80 && y<=90){
+    System.out.println("B");
+  }
+  if (y>90 && y<=100){
+    System.out.println("A");
+  }
+}
+}
+public class lesson{
+public static void main(String[] args){
+  int c=0
+  int v=0
+  while (c<10){
+    if((v%3==0) || (v%4==0)) && (v%12!=0){
+
+      System.out.println(v)
+    }
+
+
+  }
+}
+}
