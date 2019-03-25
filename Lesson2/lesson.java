607a42ff2c593dd45be57059c18c0dbdf2981481
andrewzhou16 <azhou7178@bths.edu>
2019-03-24 21:34:43 -0400

    Class Work

---
+++ Lesson2/lesson.java
@@ -0,0 +1,38 @@
+public class lesson{
+  public static void main(String[] args){
+    //System.out.println(printSum(10, 15));
+    //System.out.println(args[0]);
+    //System.out.println(args[1]);
+    int[]arr=new int[]{0,1,2,3,4,5,6,7,8,9};
+    //String[] months = new String[]{"Jan","Feb","Mar","Apr","May"};
+    //for (int i=0; i <= 3; int tmp=arr[i]; arr[i]=arr[8-i]; arr [8-i]=tmp;i++){
+      //int tmp=0;
+    //}
+    int[][] arr2=new int[][]{
+      {1,2,3,4},
+      {5,6,7,8},
+      {9,10,11,12}
+    };
+    System.out.println(arr2[1][2]);
+    for (int x=0; x<= 12; int tmp=grid.length; 
+    //for (int i=0;i<arr.length; i++){
+      //System.out.println(arr[i]);
+    }
+    //the "int"s must match
+    //"10" is the array size (how many values it can hold)
+    //the first position of the array is 0
+  /*public is the scope, void means no return value,
+  main is the name of the function*/
+  }
+  //public static int printSum(int a, int b){
+    //int total = 0;
+    //for (int i=a; i<=b; i++){
+      //total += i;
+
+    //return(total);
+
+  //in the parenthesis you put the function parameters
+  //to call a function, run the name
+
+//returning functions will pass values back to where they came from
+//arrays store multiple objects that you can access from
