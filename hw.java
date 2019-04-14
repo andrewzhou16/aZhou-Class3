b6a47580a2f126826b636168d540b904ffab1603
andrewzhou16 <48416885+andrewzhou16@users.noreply.github.com>
2019-04-13 20:47:22 -0400

    Add files via upload

--- hw.java
+++ hw.java
@@ -1,63 +1,20 @@
-public static lesson{
-public static void main (String[] args){
-  int [] arr= new int []{1,2,3,4,5,6,7,8,9};
-  arr[0]=1;
-  for (int i=0, 0<arr.length, i++)
-    {
-    int temp = arr[i];
-    arr[i] = arr[8-i];
-    arr[8-i] = temp;
+public class lesson {
+  public static void main (String[] args){
+    System.out.println()
+
+
+
+
+    class BankAccount{
+    String accountNumber=int;
+    String balanceDouble=0.0;
+    public BankAccount(int a){
+      accountNumber = x;
     }
-}
-}
-
-public static lesson{
-public static void main (String[] args){
-  int [] arr= new int []{1,2,3,4,5};
-                      {
-                        int temp = arr [i];
-                        arr[i] = arr[5-i];
-                        arr[5-i] = temp;
-                      }
-  System.out.println(arr.length[i])
-    int [] arra= new int []{6,7,8,9,10};
-                        {
-                          int temp = arra [b];
-                          arra[b] = arr[10-b];
-                          arra[10-i] = temp;
-                        }
-    System.out.println(arr.length[i])
-      int [] arraa= new int []{11,12,13,14,15};
-                          {
-                            int temp = arraa [c];
-                            arraa[c] = arraa[15-c];
-                            arraa[15-c] = temp;
-                          }
-      System.out.println(arr.length[i])
-
-}
-}
-
-public static lesson{
-public static void main (String[] args){
-  int x=55
-  while (x<55){
-    n%2==1
-    System.out.println('1');
-    n=n/2
+    public String GetAccountNumber{
+      return x;
     }
-}
-}
 
-public static lesson{
-public static void main (String[] args){
-
-}
-}
-
-public static lesson{
-public static void main (String[] args){
-  for (i=1, i<=100, i++)
-
-}
+    }
+  }
 }
