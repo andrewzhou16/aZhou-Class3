a4b022bc23e8e77317038af2ef2cd33183e246f9
andrewzhou16 <48416885+andrewzhou16@users.noreply.github.com>
2019-04-27 19:54:11 -0400

    Add files via upload

---
+++ User.java
@@ -0,0 +1,34 @@
+class User{
+  String userid = "name";
+  String password;
+  date = registerDate;
+  public void Login()
+}
+class Customer extends User{
+  public void name(){
+    System.out.println("account");
+  }
+  String address =;
+  public void address(){
+    System.out.println("Where You Live")
+  }
+  String customerId = "account";
+  public void customerId(){
+    System.out.println("CustomerId")
+  }
+  public void accountBalance(){
+    System.out.println("double")
+  }
+}
+class Administrator extends User{
+  String email = "Gmail";
+  String name = "Name";
+  public void updateProducts(){
+    +product
+  }
+}
+class Order extends Customer{
+  public void placeOrder(){
+    System.out.println("Placed Order");
+  }
+}
---
+++ student.java
@@ -0,0 +1,28 @@
+class student {
+  String name;
+  String address;
+  int numCourses = 0
+  String courses = {}
+  int grades = {}
+    public void (int u){
+    gear = ;
+  }
+  public void getName(){
+    System.out.println("name");
+  }
+  public void getAddress(){
+    System.out.println("Address");
+  }
+  public int setAddress(address){
+    return void;
+  }
+  public void addCourseGrade(){
+    return "course" || grades;
+  }
+  public void printGrades(){
+    return void;
+  }
+  public void getaverage(){
+    return "double";
+  }
+}
