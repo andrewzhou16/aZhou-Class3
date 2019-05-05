1b62e493b1f4fa6d42b15e5f19ccb795c2b3d6c3
andrewzhou16 <48416885+andrewzhou16@users.noreply.github.com>
2019-05-04 20:20:12 -0400

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,34 @@
+class Animal{
+  String name;
+  public void setName(){
+    this.name = name
+  }
+}
+class Cat extends Animal{
+  public void name(){
+    String name = NormalCat
+  }
+  public String speak(){
+    System.out.println("meow")
+  }
+}
+
+
+class Dog extends class Animal{
+  public void name(){
+    String name = NormalDog
+  }
+  public String speak(){
+    System.out.println(inpNoise);
+  }
+}
+
+class LoudDog extends class Dog{
+  public void name(){
+    String name = LoudDog
+  }
+  public String speak(){
+    String inpNoise = (inpNoise + " " + inpNoise);
+  }
+
+}
