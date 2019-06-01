123fcde3abc29a797d3a0ea801c4ee5465d248d7
andrewzhou16 <48416885+andrewzhou16@users.noreply.github.com>
2019-06-01 18:23:01 -0400

    Add files via upload

---
+++ hw.java
@@ -0,0 +1,55 @@
+public hw(){
+  int int[] array1;
+  int int[] array2;
+  int int[] a;
+  int n;
+  int left;
+  int right;
+}
+public void mergeSort(int[] a, int n){
+  int mid = n / 2;
+  int[] array1 = new int[mid];
+  int[] array2 = new int[n - mid];
+
+  for (int i = 0; i < mid; i++){
+      array1[i] = a[i];
+  }
+  for (int i = mid; i < n; i++){
+      array2[i - mid] = a[i];
+  }
+  mergeSort(array1, mid);
+  mergeSort(array2, n - mid);
+
+  merge(a, array1, array2, mid, n - mid);
+}
+
+public void merge(int[] a, int[] array1, int[] array2, int left, int right){
+  int i = 0;
+  int j = 0;
+  int k = 0;
+  while (i < left && j < right){
+      if (array1[i] <= array2[j]){
+          a[k++] = array1[i++];
+      }
+      else {
+          a[k++] = array2[j++];
+      }
+  }
+  while (i < left){
+      a[k++] = array1[i++];
+  }
+  while (j < right){
+      a[k++] = array2[j++];
+  }
+}
+
+public void binarySearch(int n){
+  for(int j = 0; j < array.length - 1; j++){
+    if(array[i] == n && i > array.length - 1/2){
+      for(int i = array.length - 1; i > array.length -1/){
+        array2[i] = array[i];
+        array = array2;
+      }
+    }
+  }
+}
